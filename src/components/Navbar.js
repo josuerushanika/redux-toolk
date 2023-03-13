cdc1ba660bb92e2ca00436bd0fc0470436b8b31a
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-13 20:46:47 +0200

    Displaying card

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
-import {CartIcon} from '../icons'
+import {CartIcon,Testing} from '../icons'
 import { useSelector } from 'react-redux'
 
 import React from 'react'
 
 const Navbar = () => {
-  const amount= useSelector((store) => store.cart.amount);
+  const {amount} = useSelector((store) => store.cart);
  
   return (
     <nav>
       <div className='nav-center'>
         <h3>redux toolkit</h3>
         <div className='nav-container'>
+          
             <CartIcon />
             <div className='amount-container'> 
              <p className='total-amount'>{amount}</p>
