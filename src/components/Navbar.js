938b5d9db40b354152b95db69cb6f823f799d935
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-13 19:23:12 +0200

    Console.log the store in the dev tool

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,20 @@ import { useSelector } from 'react-redux'
 import React from 'react'
 
 const Navbar = () => {
+  console.log(
+   useSelector((store) => {
+    console.log(store);
+   })
+  );
   return (
     <nav>
       <div className='nav-center'>
         <h3>redux toolkit</h3>
         <div className='nav-container'>
             <CartIcon />
+            <div className='amount-container'> 
+             <p className='total-amount'>0</p>
+            </div>
         </div>
       </div>
     </nav>
