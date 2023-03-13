0f7c29dcc12918a2a855b2e7375e7e4a77118ac4
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-13 23:55:53 +0200

    Adding decraisse and incrise

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -12,13 +12,39 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     clearCart: (state) => {
-      state.cartItems = [];
+      // state.cartItems = [];
+      return { cartItems: []}
+    },
+
+    removeItem :(state,action) =>{
+      // console.log(action);
+      const itemId = action.payload
+      state.cartItems = state.cartItems.filter((item)=>item.id !== itemId); 
+    },
+
+    // increase:(state,{payload}) => {
+    //   const cartItem = state.cartItems.find((item)=>item.id === payload)
+    //   cartItem.amount = cartItem.amount + 1;
+    // },
+
+    increase:(state,{payload}) => {
+      const cartItem = state.cartItems.find((item)=>item.id === payload.id)
+        cartItem.amount = cartItem.amount + 1; 
+    },
+
+    // decrease:(state,{payload}) => {
+    //   const cartItem = state.cartItems.find((item)=>item.id === payload)
+    //   cartItem.amount = cartItem.amount - 1;
+    // }
+    decrease:(state,{payload}) => {
+      const cartItem = state.cartItems.find((item)=>item.id === payload.id)
+        cartItem.amount = cartItem.amount - 1;
     }
-  }
+  },
 });
 
 // console.log(cartSlice);
 
-export const {clearCart} = cartSlice.actions;
+export const {clearCart,removeItem,increase,decrease} = cartSlice.actions;
 
 export default cartSlice.reducer
