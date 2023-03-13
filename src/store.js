3365a07b5e10ae485f6de1667f886ebac01bc843
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-13 18:07:54 +0200

    Adding store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './features/cart/cartSlice';
-import modalReducer from './features/modal/modalSlice';
+import { configureStore } from "@reduxjs/toolkit";
+
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    modal: modalReducer,
-  },
+  reducer: {},
 });
